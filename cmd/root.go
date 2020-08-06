e17a279bc8d4be44cae39f14c937d2d48da1edba
hyprd <yumether@gmail.com>
2020-08-06 15:50:34 +1200

    Error handling, fetcher command working

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 
@@ -12,23 +13,36 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "cerulean",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 {
+			println("Fetching mp3...")
+			root := "youtube-dl"
+			r_ignore_error := "-i"
+			r_metadata := "--add-metadata"
+			r_extract_audio := "-x"
+			r_audio_format := "--audio-format"
+			r_audio_type := "mp3"
+			r_manifest := "--youtube-skip-dash-manifest"
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+			track := args[0]
+			run := exec.Command(root, r_ignore_error, r_metadata, r_extract_audio, r_audio_format, r_audio_type, r_manifest, track)
+			stdout, err := run.Output()
+			if err != nil {
+				println("Failure - check your YouTube URL (everything after ?v=)")
+				//println(err.Error())
+				return
+			}
+			println(string(stdout))
+		} else {
+			println("Specify a valid YouTube video URL (everything after `v?=`)")
+		}
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -38,39 +52,23 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cerulean.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-
-		// Search config in home directory with name ".cerulean" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".cerulean")
 	}
-
 	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
